e433c7f83eca2977cb859663d839f2942c1e1f31
Ales Raszka <araszka@redhat.com>
2024-11-07 14:16:42 +0100

    Fix pylint violation in tests
    Signed-off-by: Ales Raszka <araszka@redhat.com>

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,12 @@
+"""A unit test module"""
+
+
 def test_example_of_successful_test():
+    """A test that always passes"""
     assert True
 
 
 def test_example_of_failed_test():
+    """A test that always fails"""
     # Fix or remove this test
     assert False
